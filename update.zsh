e48a712daa544e6bd73b72102dde1d71fd0215a7
Max <5474655+maxzinkus@users.noreply.github.com>
2018-10-27 20:08:43 -0400

    Comments in update.zsh, standardize use of pushd/popd

--- update.zsh
+++ update.zsh
@@ -1,15 +1,34 @@
 #!/usr/bin/zsh
 
+# One script to bring them all and in the darkness update them
+
+# Update ubuntu packages and clean up
 sudo apt update ; sudo apt full-upgrade ; sudo apt autoremove ; sudo apt autoclean
 sudo apt autoremove ; sudo apt autoclean
+
+# Update oh-my-zsh (and its built-in themes and plugins)
 upgrade_oh_my_zsh
+
+# Update fzf
 cd ~/.fzf ; git pull ; ./install --key-bindings --no-completion --no-bash --no-fish --no-update-rc ; cd -
+
+# Update vim plugins (using git submodules)
 vim-plug-update
-pushd
-cd ~/.oh-my-zsh/custom/plugins
+
+# Check for diff-so-fancy updates by comparing hashes, and update if needed
+local dsfurl="https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy"
+local dsftmp=$(mktemp)
+wget --no-verbose -O $dsftmp $dsfurl
+if [ $(sha256sum $dsftmp | awk '{ print $1 }') != $(sha256sum ~/.local/bin/diff-so-fancy | awk '{ print $1 }') ]
+then echo "Updating diff-so-fancy" ; cp $dsftmp ~/.local/bin/diff-so-fancy ; chmod +x ~/.local/bin/diff-so-fancy
+fi
+rm $dsftmp
+
+# Update custom oh-my-zsh plugins (sadly doesn't play nice with submodules)
+pushd ~/.oh-my-zsh/custom/plugins
 for plugin in $(ls)
 do if [ "$plugin" != "example" ]
-    then cd "$plugin" ; echo "Updating $plugin" ; git pull ; cd ..
+    then pushd "$plugin" ; echo "Updating $plugin" ; git pull ; popd
     fi
 done
 popd
