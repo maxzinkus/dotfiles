a7ec144e68b43433f4eb13726afb43ccde75b551
Max <5474655+maxzinkus@users.noreply.github.com>
2020-03-17 12:12:07 -0400

    Only run backup if we can reach backup host

--- backup.zsh
+++ backup.zsh
@@ -1,3 +1,7 @@
 #!/usr/bin/zsh
 
+if ! ssh -q tower -o RemoteCommand="exit" -o ConnectTimeout="1" >/dev/null 2>&1
+then echo "Can't backup, no route to backup host." 1>&2 ; exit 1
+fi
+
 rsync --exclude "lost+found" -avz /mnt/sda1 tower:~/BACKUP_LAPTOP
