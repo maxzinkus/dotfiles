23458319804a55d0a01faa520c02736136a91b40
Max <5474655+maxzinkus@users.noreply.github.com>
2020-10-04 14:39:42 -0400

    Don't compress encrypted backups, just a slowdown

--- backup.zsh
+++ backup.zsh
@@ -4,4 +4,4 @@ if ! ssh -q tower -o RemoteCommand="exit" -o ConnectTimeout="1" >/dev/null 2>&1
 then echo "Can't backup, no route to backup host." 1>&2 ; exit 1
 fi
 
-rsync --progress --exclude "lost+found" --compress-level 9 -avz /mnt/sda1 backup:~/BACKUP_LAPTOP && touch ~/.last-backup-run && exit 0 || exit 1
+rsync --progress --exclude "lost+found" -av --compress-level=0 /mnt/sda1 backup:~/BACKUP_LAPTOP && touch ~/.last-backup-run && exit 0 || exit 1
