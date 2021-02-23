7d87a4144e69f4cdbe4431008f54dee700511ef2
Max <5474655+maxzinkus@users.noreply.github.com>
2021-02-22 20:39:06 -0500

    Don't need rsync parallelism with fast internet

--- backup.zsh
+++ backup.zsh
@@ -1,10 +1,13 @@
 #!/usr/bin/zsh
 
-if ! ssh -q tower -o RemoteCommand="exit" -o ConnectTimeout="1" >/dev/null 2>&1
+if ! ssh -q backup -o RemoteCommand="exit" -o ConnectTimeout="1" >/dev/null 2>&1
 then echo "Can't backup, no route to backup host." 1>&2 ; exit 1
 fi
 
-ls /mnt/sda1 | xargs -n1 -P4 -I% rsync --exclude "lost+found" -auv4 --partial --compress-level=0 /mnt/sda1/% backup:~/BACKUP_LAPTOP
+BACKUP_DIR="/media/user/Backups"
+
+#ls $BACKUP_DIR | xargs -n1 -P4 -I% rsync --exclude "lost+found" --exclude ".Trash-1000" -auv --partial --compress-level=0 "$BACKUP_DIR"/% backup:~/BACKUP_LAPTOP
+rsync --progress --exclude "lost+found" --exclude ".Trash-1000" -auv --partial --compress-level=0 "$BACKUP_DIR" backup:~/BACKUP_LAPTOP
 
 ret=$?
 if [ $ret -eq 0 ]
